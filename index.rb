437a278a962d5d38912338f1c47cbd3e3e851b95
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-02-26 15:54:23 +0100

    Add content directory

--- index.rb
+++ index.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+ROOT_DIR = File.dirname(__FILE__)
+
+CONTENT_DIR = File.join(ROOT_DIR, 'content')
+
 get '/' do
-  'Hello World'
+  if Dir.exist?(CONTENT_DIR)
+    Dir.each_child(CONTENT_DIR).to_a.join(', ')
+  else
+    'Not found'
+  end
 end
